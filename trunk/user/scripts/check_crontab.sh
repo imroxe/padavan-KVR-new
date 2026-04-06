b5181e87313525b622cfa3feef4586baadd36e5a
Hans <34638380+imroxe@users.noreply.github.com>
2026-04-06 17:51:34 +0800

    Refactor crontab entry addition in check_crontab.sh
    Refactor crontab entry addition to use a block for echoing multiple lines.

--- trunk/user/scripts/check_crontab.sh
+++ trunk/user/scripts/check_crontab.sh
@@ -5,6 +5,13 @@
 cd /etc/storage/
 exp=`echo "$1 $2 $3 $4 $5" |sed 's/a/\*/g'`
 if [ ! -f "cron/crontabs/admin" ] || [ -z "$(cat cron/crontabs/admin |grep $6)" ]; then
-	echo "$exp /usr/bin/$6 > /dev/null 2>&1" >> cron/crontabs/admin && exit 1
+	# echo "$exp /usr/bin/$6 > /dev/null 2>&1" >> cron/crontabs/admin && exit 1
+	{
+	    echo "$exp /usr/bin/$6 > /dev/null 2>&1"
+		echo ""
+		echo "# update github host"
+		echo "0 9 * * * logger -t "【SmartDNS】" "准备更新github可访问ip" && /etc/storage/update-github-host.sh >/dev/null 2>&1"
+		echo ""
+	} >> cron/crontabs/admin && exit 1
 fi
 exit 0
